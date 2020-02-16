b7bee6690a5ee3cf22d8697bb755547f6edc85d8
KoheiYamashita <k-yamashita@kitproject.info>
2020-02-16 22:10:46 +0900

    Create initial screen

---
+++ lib/screens/initial_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:idea_note/config/preferences_key.dart';
+import 'package:idea_note/route.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class InitialScreen extends StatefulWidget {
+  @override
+  _InitialScreenState createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  final PageController _pageController = PageController();
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          PageView(
+            controller: _pageController,
+            children: <Widget>[
+              const Center(
+                child: Text('チュートリアル1'),
+              ),
+              const Center(
+                child: Text('チュートリアル2'),
+              ),
+              const Center(
+                child: Text('チュートリアル3'),
+              ),
+              Center(
+                child: RaisedButton(
+                  child: Text('完了', style: themeData.textTheme.button),
+                  color: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  onPressed: () async {
+                    final prefs = await SharedPreferences.getInstance();
+                    await prefs.setBool(PreferencesKey.setUpCompleted, true);
+                    await Navigator.of(context)
+                        .pushReplacementNamed(RoutePath.home);
+                  },
+                ),
+              ),
+            ],
+          ),
+          Center(
+            child: Column(
+              children: <Widget>[
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: SmoothPageIndicator(
+                    controller: _pageController, // PageController
+                    count: 4,
+                    effect: const WormEffect(), // your preferred effect
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -3,7 +3,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:idea_note/config/assets.dart';
+import 'package:idea_note/config/preferences_key.dart';
 import 'package:idea_note/route.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -11,12 +13,22 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreen extends State<SplashScreen> {
+  bool _isSetUpCompleted = false;
+  bool _isLoadPrefs = false;
+  bool _isSplashTimeEnds = false;
+  int _numberOfStartUps = 0;
   Timer _timer;
 
   @override
   void initState() {
     super.initState();
-    _timer = Timer(const Duration(seconds: 2), _openScreen);
+    _timer = Timer(const Duration(seconds: 2), () {
+      _isSplashTimeEnds = true;
+      _openScreen();
+    });
+    _getPrefs().then((_) {
+      _openScreen();
+    });
   }
 
   @override
@@ -27,8 +39,23 @@ class _SplashScreen extends State<SplashScreen> {
     super.dispose();
   }
 
+  Future<void> _getPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    _numberOfStartUps = prefs.getInt(PreferencesKey.numberOfStartUps) ?? 0;
+    await prefs.setInt(PreferencesKey.numberOfStartUps, _numberOfStartUps + 1);
+    _isSetUpCompleted = prefs.getBool(PreferencesKey.setUpCompleted) ?? false;
+    _isLoadPrefs = true;
+  }
+
   void _openScreen() {
-    Navigator.of(context).pushReplacementNamed(RoutePath.home);
+    if (!_isLoadPrefs || !_isSplashTimeEnds) {
+      return;
+    }
+    if (_numberOfStartUps > 0 && _isSetUpCompleted) {
+      Navigator.of(context).pushReplacementNamed(RoutePath.home);
+    } else {
+      Navigator.of(context).pushReplacementNamed(RoutePath.initial);
+    }
   }
 
   @override
